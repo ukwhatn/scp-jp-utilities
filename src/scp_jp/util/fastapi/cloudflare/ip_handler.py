3e5f1aca3e608056244778fce42f7b3fcb1dd933
Yuki Watanabe <ukwhatn@gmail.com>
2025-01-23 01:29:20 +0900

    release: 1.0.0

--- src/scp_jp/util/fastapi/cloudflare/ip_handler.py
+++ src/scp_jp/util/fastapi/cloudflare/ip_handler.py
@@ -3,7 +3,8 @@ from fastapi import Request
 
 class CFIPAddressHandler:
     @staticmethod
-    def get_client_ip(request: Request) -> str:
+    def get_client_ip(request: Request) -> str | None:
         if "CF-Connecting-IP" in request.headers:
             return request.headers["CF-Connecting-IP"]
-        return request.client.host
+
+        return request.client.host if request.client is not None else None
